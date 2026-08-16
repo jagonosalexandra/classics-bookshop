34977c44aa84f648853033bd0b5d57ba545e8008
unknown <jagonosalexandra@gmail.com>
2026-08-16 13:15:34 +0800

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import "./index.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link className="logo" to="/">
+        Classics Bookshop
+      </Link>
+
+      <ul>
+        <NavLink className="navlink" to="/">
+          Home
+        </NavLink>
+        <NavLink className="navlink" to="/about">
+          About
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
