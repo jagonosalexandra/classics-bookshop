dd767800540176a5d5cc7ad704869cf4a9d25b2b
unknown <jagonosalexandra@gmail.com>
2026-08-17 11:31:40 +0800

    Add book not found fallback page

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,11 +1,30 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { books } from "../assets/books";
+import Button from "../components/Button";
 
 function ProductDetail() {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
 
   const book = books.find((book) => book._id === id);
   const relBooks = books.filter((book) => book._id !== id);
+
+  if (!book)
+    return (
+      <div className="book-not-found-container">
+        <p>
+          <span>Book Not Found</span>
+          Sorry, we couldn't find the book you're looking for. The product ID
+          you have entered may be incorrect.
+        </p>
+
+        <Button
+          label="Return to Home"
+          variant="secondary"
+          onClick={() => navigate("/")}
+        />
+      </div>
+    );
 
   return (
     book && (
@@ -39,7 +58,6 @@ function ProductDetail() {
               <Link
                 key={book._id}
                 to={`/product/${book._id}`}
-                onClick={() => scrollTo(0, 0)}
                 className="book-card"
               >
                 <img className="book-image" src={book.image} alt={book.title} />
