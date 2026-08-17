f4d2aa4a6644476addf6c8facbd7f051c63aa28b
unknown <jagonosalexandra@gmail.com>
2026-08-17 11:38:01 +0800

    Add Not Found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+
 function NotFound() {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found-container">
+      <p>
+        <span className="not-found-text">Page Not Found</span>
+        Sorry, we couldn't find the page you were looking for. It may have
+        movved or the web address you entered was incorrect.
+      </p>
+
+      <Button
+        label="Return to Home"
+        variant="secondary"
+        onClick={() => navigate("/")}
+      />
+    </div>
+  );
 }
 
 export default NotFound;
