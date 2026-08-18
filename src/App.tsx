01994b252a2335b90738549f54002d05ed31beb4
unknown <jagonosalexandra@gmail.com>
2026-08-18 23:21:21 +0800

    Add ProtectedRoute route with isAuthenticated state variable

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,38 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import ProductDetail from "./pages/ProductDetail";
 import ScrollToTop from "./components/ScrollToTop";
+import Login from "./pages/Login";
+import { useState } from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <ScrollToTop />
-      <Navbar />
+      <Navbar
+        isAuthenticated={isAuthenticated}
+        onLogout={() => setIsAuthenticated(false)}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="*" element={<NotFound />} />
+        <Route
+          path="/login"
+          element={<Login onLoginSuccess={() => setIsAuthenticated(true)} />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
