e8ff52651a9268f3c773d5fc25e975ea20e748ce
unknown <jagonosalexandra@gmail.com>
2026-08-19 09:34:51 +0800

    Remove book && (...) to cleanup second return statament

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -27,52 +27,50 @@ function ProductDetail() {
     );
 
   return (
-    book && (
-      <div>
-        <div className="product-detail-container">
-          <img src={book.image} alt={book.title} />
+    <div>
+      <div className="product-detail-container">
+        <img src={book.image} alt={book.title} />
 
-          <div className="book-details">
-            <h2>
-              {book.title}
-              <span>By {book.author}</span>
-            </h2>
+        <div className="book-details">
+          <h2>
+            {book.title}
+            <span>By {book.author}</span>
+          </h2>
 
-            <p>
-              ID: {book._id} | {book.pageCount} pages
-            </p>
-            <p className="book-description">
-              <span>Book Description</span>
-              {book.description}
-            </p>
+          <p>
+            ID: {book._id} | {book.pageCount} pages
+          </p>
+          <p className="book-description">
+            <span>Book Description</span>
+            {book.description}
+          </p>
 
-            <p>₱{book.price.toFixed(2)}</p>
-          </div>
-        </div>
-
-        <div className="other-books-container">
-          <h2>Related Poducts</h2>
-
-          <div className="books-container">
-            {relBooks.map((book) => (
-              <Link
-                key={book._id}
-                to={`/product/${book._id}`}
-                className="book-card"
-              >
-                <img className="book-image" src={book.image} alt={book.title} />
-                <div>
-                  <p className="book-title">{book.title}</p>
-                  <span className="book-author">By {book.author}</span>
-
-                  <p className="book-price">PHP {book.price.toFixed(2)}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
+          <p>₱{book.price.toFixed(2)}</p>
         </div>
       </div>
-    )
+
+      <div className="other-books-container">
+        <h2>Related Poducts</h2>
+
+        <div className="books-container">
+          {relBooks.map((book) => (
+            <Link
+              key={book._id}
+              to={`/product/${book._id}`}
+              className="book-card"
+            >
+              <img className="book-image" src={book.image} alt={book.title} />
+              <div>
+                <p className="book-title">{book.title}</p>
+                <span className="book-author">By {book.author}</span>
+
+                <p className="book-price">PHP {book.price.toFixed(2)}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
